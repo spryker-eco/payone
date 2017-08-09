32a2f02599f4251449981d37a5fc969d19ce6e54
Eugene Varava <ievgen.varava@spryker.com>
2017-08-09 18:42:34 +0200

    ECO-688: Fixed code review suggestions (removed console output, added missing space between method parameters.)

--- assets/Yves/js/modules/payment-method.js
+++ assets/Yves/js/modules/payment-method.js
@@ -40,7 +40,7 @@ function initHostedIframe(config) {
     document.paymentform = document.getElementById(config.formId);
     var $form = $(config.formSelector);
     var clientApiConfig = JSON.parse($form.find(config.clientApiConfigInput).val());
-    var language = $form.find(config.languageInput).val().substr(0,2);
+    var language = $form.find(config.languageInput).val().substr(0, 2);
     config.hostedIframeConfig.language = Payone.ClientApi.Language[language];
 
     var iframes = new Payone.ClientApi.HostedIFrames(config.hostedIframeConfig, clientApiConfig);
@@ -71,13 +71,11 @@ function check(iframes, config) {
     if (iframes.isComplete() && $(config.cardholderInput).val()) {
         iframes.creditCardCheck('checkCallback');
     } else {
-        console.debug("The form is not complete");
         $(config.errorDivSelector).text(config.hostedIframeConfig.language.transactionRejected);
     }
 }
 
 function checkCallback(response) {
-    console.debug(response);
     if (response.status === "VALID") {
         document.getElementById("pseudocardpan").value = response.pseudocardpan;
         document.paymentform.submit();
