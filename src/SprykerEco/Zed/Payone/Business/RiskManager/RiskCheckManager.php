9f3f44b09a7b43cba241b49b549c7f3e69cdadad
vhrychenko <aptah199494@gmail.com>
2018-11-22 17:39:45 +0200

    eco-2124 Fix not camelCase strings

--- src/SprykerEco/Zed/Payone/Business/RiskManager/RiskCheckManager.php
+++ src/SprykerEco/Zed/Payone/Business/RiskManager/RiskCheckManager.php
@@ -62,8 +62,8 @@ class RiskCheckManager implements RiskCheckManagerInterface
         $addressCheckResponseTransfer->setStreetNumber($responseContainer->getStreetNumber());
         $addressCheckResponseTransfer->setZip($responseContainer->getZip());
         $addressCheckResponseTransfer->setCity($responseContainer->getCity());
-        $addressCheckResponseTransfer->setSecstatus($responseContainer->getSecstatus());
-        $addressCheckResponseTransfer->setPersonstatus($responseContainer->getPersonstatus());
+        $addressCheckResponseTransfer->setSecStatus($responseContainer->getSecstatus());
+        $addressCheckResponseTransfer->setPersonStatus($responseContainer->getPersonstatus());
 
         if (!is_null($responseContainer->getCustomermessage())) {
             $addressCheckResponseTransfer->setCustomerMessage($responseContainer->getCustomermessage());
@@ -91,7 +91,7 @@ class RiskCheckManager implements RiskCheckManagerInterface
 
         if (!$responseContainer->isError()) {
             $consumerScoreResponseTransfer->setScore($responseContainer->getScore());
-            $consumerScoreResponseTransfer->setScorevalue($responseContainer->getScorevalue());
+            $consumerScoreResponseTransfer->setScoreValue($responseContainer->getScorevalue());
 
             return $consumerScoreResponseTransfer;
         }
