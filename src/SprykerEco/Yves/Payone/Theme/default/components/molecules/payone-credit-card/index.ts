24ffbc3ddc5bb5417baca59ee90d6001dd0389ba
maslov <dev.maslov.a@gmail.com>
2020-07-16 16:24:59 +0300

    TE-6081: Introduced webpackChunkName commentary.

--- src/SprykerEco/Yves/Payone/Theme/default/components/molecules/payone-credit-card/index.ts
+++ src/SprykerEco/Yves/Payone/Theme/default/components/molecules/payone-credit-card/index.ts
@@ -1,2 +1,5 @@
 import register from 'ShopUi/app/registry';
-export default register('payone-credit-card', () => import(/* webpackMode: "lazy" */'./payone-credit-card'));
+export default register('payone-credit-card', () => import(
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "payone-credit-card" */
+    './payone-credit-card'));
--- src/SprykerEco/Yves/Payone/Theme/default/components/molecules/payone-direct-debit/index.ts
+++ src/SprykerEco/Yves/Payone/Theme/default/components/molecules/payone-direct-debit/index.ts
@@ -1,2 +1,5 @@
 import register from 'ShopUi/app/registry';
-export default register('payone-direct-debit', () => import(/* webpackMode: "lazy" */'./payone-direct-debit'));
+export default register('payone-direct-debit', () => import(
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "payone-direct-debit" */
+    './payone-direct-debit'));
