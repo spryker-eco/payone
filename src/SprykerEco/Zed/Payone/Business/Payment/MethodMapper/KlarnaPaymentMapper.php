4a67d83e044fc2c482acfa67adfb55eee84cfb6c
Anna Kotiuk <anna.kotiuk@spryker.com>
2021-03-30 13:44:33 +0300

    ECO-3645: fix capture for refund

--- src/SprykerEco/Zed/Payone/Business/Payment/MethodMapper/KlarnaPaymentMapper.php
+++ src/SprykerEco/Zed/Payone/Business/Payment/MethodMapper/KlarnaPaymentMapper.php
@@ -95,6 +95,8 @@ class KlarnaPaymentMapper extends AbstractMapper implements KlarnaPaymentMapperI
         $captureContainer->setCurrency($this->getStandardParameter()->getCurrency());
         $captureContainer->setTxid($paymentEntity->getTransactionId());
         $captureContainer->setCaptureMode(PayoneApiConstants::CAPTURE_MODE_NOTCOMPLETED);
+        $sequenceNumber = $this->getNextSequenceNumber($paymentEntity->getTransactionId());
+        $captureContainer->setSequenceNumber($sequenceNumber);
 
         return $captureContainer;
     }
