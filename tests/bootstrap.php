101f237ffaff1c84364049d7cd522f7605607e19
Aleksey Lunin <aleksey.lunin@spryker.com>
2021-11-26 15:54:20 +0300

    Adjusted ci and config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use Spryker\Shared\Config\Config;
-
 if (!defined('MODULE_ROOT_DIR')) {
     define('MODULE_ROOT_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR);
 }
@@ -18,42 +16,3 @@ if (!defined('APPLICATION_STORE')) {
 if (!defined('APPLICATION_ENV')) {
     define('APPLICATION_ENV', 'dev');
 }
-
-// Copy config files
-$configSharedTargetDirectory = APPLICATION_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR . 'Shared' . DIRECTORY_SEPARATOR;
-if (!is_dir($configSharedTargetDirectory)) {
-    mkdir($configSharedTargetDirectory, 0777, true);
-}
-
-$configZedTargetDirectory = APPLICATION_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR . 'Zed' . DIRECTORY_SEPARATOR;
-if (!is_dir($configZedTargetDirectory)) {
-    mkdir($configZedTargetDirectory, 0777, true);
-}
-
-spl_autoload_register(function ($className) {
-    if (strrpos($className, 'Transfer') === false) {
-        return false;
-    }
-
-    $classNameParts = explode('\\', $className);
-
-    $transferFileName = implode(DIRECTORY_SEPARATOR, $classNameParts) . '.php';
-    $transferFilePath = APPLICATION_ROOT_DIR . 'src' . DIRECTORY_SEPARATOR . $transferFileName;
-
-    if (!file_exists($transferFilePath)) {
-        return false;
-    }
-
-    require_once $transferFilePath;
-
-    return true;
-});
-
-$configSourceDirectory = MODULE_ROOT_DIR . 'ci' . DIRECTORY_SEPARATOR;
-copy($configSourceDirectory . 'config_default.php', $configSharedTargetDirectory . 'config_default.php');
-copy($configSourceDirectory . 'default_store.php', $configSharedTargetDirectory . 'default_store.php');
-
-copy($configSourceDirectory . 'stores.php', $configSharedTargetDirectory . 'stores.php');
-
-$config = Config::getInstance();
-$config->init();
