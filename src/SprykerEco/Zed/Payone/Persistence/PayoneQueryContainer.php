4b54e797e16a24d9020450f7fcd71d24078f5af9
Dan Miniyarov <dan.miniyarov@spryker.com>
2022-06-21 17:22:09 +0300

    SUPESC-240: reverted a method signature change in query container

--- src/SprykerEco/Zed/Payone/Persistence/PayoneQueryContainer.php
+++ src/SprykerEco/Zed/Payone/Persistence/PayoneQueryContainer.php
@@ -7,6 +7,7 @@
 
 namespace SprykerEco\Zed\Payone\Persistence;
 
+use Orm\Zed\Payone\Persistence\SpyPaymentPayoneApiLogQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
 use Spryker\Zed\Kernel\Persistence\AbstractQueryContainer;
 use SprykerEco\Shared\Payone\PayoneApiConstants;
@@ -25,7 +26,7 @@ class PayoneQueryContainer extends AbstractQueryContainer implements PayoneQuery
      *
      * @return \Orm\Zed\Payone\Persistence\SpyPaymentPayoneApiLogQuery
      */
-    public function createCurrentSequenceNumberQuery(int $transactionId)
+    public function createCurrentSequenceNumberQuery(int $transactionId): SpyPaymentPayoneApiLogQuery
     {
         $query = $this->getFactory()->createPaymentPayoneApiLogQuery();
         $query->filterByTransactionId($transactionId)
